1367d4e16f3c81023a297e07b3e160ef9df72b6d
Jbassard <jonathanacciarito@gmail.com>
2026-06-19 01:43:06 -0400

    file manager now has buttons for going back and jumping straight to /, honestly you can navigate without typing in the input which is golden no lie. next I want to try creating files and folders in file manager and after that, uploads from device

--- client/src/applications/FileManager/FileManager.tsx
+++ client/src/applications/FileManager/FileManager.tsx
@@ -4,6 +4,8 @@ import { VFS_ROOT } from "../../constants/constants";
 import { ls } from "../../vfs-actions/ls";
 import { getFileIcon } from "../../helpers/getFileIcon";
 import { formatBytes } from "../../helpers/formatBytes";
+import homeIcon from "../../assets/icons/home.svg";
+import backIcon from "../../assets/icons/go-back.svg";
 
 export default function FileManager({
   startPath,
@@ -39,6 +41,18 @@ export default function FileManager({
   return (
     <div className="file-manager">
       <div className="form-container">
+        <div className="options-bar">
+          <p className="option">
+            <PlusIcon />
+            <FileIcon />
+          </p>
+          <p className="option">
+            <PlusIcon />
+            <FolderIcon />
+          </p>
+          <p className="option">Upload...</p>
+        </div>
+
         <input
           type="text"
           name="path-input"
@@ -46,7 +60,30 @@ export default function FileManager({
           value={pathInputState}
           onChange={(e) => setPathInputState(e.target.value)}
         />
-        <p className="status-bar">Entries Found: {entriesFound.length}</p>
+        <div className="status-bar">
+          <p className="entries-found">Entries Found: {entriesFound.length}</p>
+          <div>
+            <div
+              className="go-back"
+              onClick={() => {
+                const trimmed = pathFound.endsWith("/")
+                  ? pathFound.slice(0, -1)
+                  : pathFound;
+
+                const lastSlash = trimmed.lastIndexOf("/");
+
+                setPathInputState(
+                  lastSlash <= 0 ? "/" : `${trimmed.slice(0, lastSlash)}/`,
+                );
+              }}
+            >
+              <img src={backIcon} />
+            </div>
+            <div className="go-home" onClick={() => setPathInputState("/")}>
+              <img src={homeIcon} />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="entries-container">
         {entriesFound.length > 0 &&
@@ -101,5 +138,92 @@ export default function FileManager({
           )}
       </div>
     </div>
+  );
+}
+
+function PlusIcon() {
+  return (
+    <svg
+      className="plus-icon"
+      fill="#ffffff"
+      width="64px"
+      height="64px"
+      viewBox="0 0 10 10"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <g>
+          {" "}
+          <polygon points="9 4.1 5.9 4.1 5.9 1 4.1 1 4.1 4.1 1 4.1 1 5.9 4.1 5.9 4.1 9 5.9 9 5.9 5.9 9 5.9 9 4.1" />{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
+}
+
+function FileIcon() {
+  return (
+    <svg
+      className="file-icon"
+      fill="#ffffff"
+      width="75"
+      height="75"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="matrix(-1, 0, 0, 1, 0, 0)"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <path fillRule="evenodd" d="M8,1V6H3v9H13V1ZM7,1,3,5H7Z" />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+function FolderIcon() {
+  return (
+    <svg
+      className="folder-icon"
+      fill="#ffffff"
+      width="75px"
+      height="75px"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="matrix(1, 0, 0, 1, 0, 0)"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <g>
+          {" "}
+          <path d="M9,5A2.51,2.51,0,0,0,6.5,3H1V14H15V5Z" />{" "}
+        </g>{" "}
+      </g>
+    </svg>
   );
 }
