2ce9feaaa0679a076d3b386c4e7eea5573cd1415
Jbassard <jonathanacciarito@gmail.com>
2026-07-19 21:51:05 -0400

    audio player isn't working on mobile, I'm assuming it has to do with autoplay

--- client/src/applications/AudioPlayer/AudioPlayer.tsx
+++ client/src/applications/AudioPlayer/AudioPlayer.tsx
@@ -156,7 +156,7 @@ export default function AudioPlayer({
           }}
         />
 
-        <audio ref={audioRef} src={audioSrc ?? undefined} controls autoPlay />
+        <audio ref={audioRef} src={audioSrc ?? undefined} controls />
       </div>
 
       <div className="file-path-display">
